5c00b54b6c7b8e83b85b7ae82413cc2de991aec9
Rahimkhan <romakaibxanov@MacBook-Pro-Rahimkhan.local>
2022-07-31 04:39:20 +0400

    added lesson_15

--- js/lesson_15.js
+++ js/lesson_15.js
@@ -1,18 +1,28 @@
     "use strict";
 
-    //Задача 1js.Pm.Bs.ID.1
+    // №1
 
-    //  Переделайте этот код так, чтобы в нем операции инкремента и декремента:
+    // Создайте переменную str и присвойте ей строку '!!!'. Выведите значение этой переменной на экран.
 
-    // let num = 10;
-    // num = num + 1;
-    // num = num + 1;
-    // num = num - 1;
-    // alert(num);
+    let str = '!!!';
 
-    let num = 10;
-    num++;
-    num++;
-    num--;
-    alert(num);
+    alert(str);
+
+    // №2
+
+    // Создайте переменную с текстом 'java' и переменную с текстом 'script'. С помощью этих переменных и операции сложения строк выведите на экран строку 'javascript'.
+
+    let languageOne = 'java';
+    let languageTwo = 'script';
+
+    alert(languageOne + languageTwo);
+
+    // №3
+
+    // Создайте переменную с текстом 'hello' и переменную с текстом 'world'. С помощью этих переменных и операции сложения строк выведите на экран строку 'hello world'.
+
+    let hello = 'hello';
+    let world = 'world';
+
+    alert(hello + ' ' + world);
 
