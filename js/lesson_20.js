b599a1256a49d097ab845d2381be467feb525822
Rahimkhan <romakaibxanov@MacBook-Pro-Rahimkhan.local>
2022-08-04 03:23:20 +0400

    added lesson_20

--- js/lesson_20.js
+++ js/lesson_20.js
@@ -1,3 +1,13 @@
     "use strict";
 
-    
+    // №1
+
+    // Сделайте константу PI и с ее помощью вычислите длину какой-нибудь окружности по ее радиусу.
+
+    const PI = 3.14;
+    let radius = 0.7;
+    let diameter = radius * 2;
+
+    console.log(PI * diameter);
+    console.log(3.14 * 2 * 0.7);
+
